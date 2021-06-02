7ac59458534c226463b9f435712df8a5621138f1
Michael Allaway <m.j.allaway@bham.ac.uk>
2021-06-02 11:01:19 +0100

    various tweaks

--- django/researchdata/urls.py
+++ django/researchdata/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.StoryListView.as_view(), name='story-list'),
     path('create/', views.StoryCreateView.as_view(), name='story-create'),
-    path('map/', views.StoryListView.as_view(), name='story-list'),
 ]
--- django/researchdata/views.py
+++ django/researchdata/views.py
@@ -13,7 +13,8 @@ class StoryCreateView(CreateView):
 
 class StoryListView(ListView):
     """
-    Class-based view to show the list template
+    Class-based view to show the story list template
+    Stories are grouped by location, which is why model = Location
     """
     template_name = 'researchdata/story-list.html'
-    model = models.Story
+    model = models.Location
