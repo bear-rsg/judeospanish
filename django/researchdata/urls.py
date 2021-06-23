c24e2e5269a4f56ef7fe0cbbfd1857c419c2b7ef
Michael Allaway <m.j.allaway@bham.ac.uk>
2021-06-23 14:07:15 +0100

    fixed f8

--- django/researchdata/urls.py
+++ django/researchdata/urls.py
@@ -3,11 +3,23 @@ from . import views
 
 urlpatterns = [
     # Story
-    path('stories/', views.StoryListView.as_view(), name='story-list'),
-    path('stories/<int:pk>/', views.StoryDetailView.as_view(), name='story-detail'),
-    path('stories/create/', views.StoryCreateView.as_view(), name='story-create'),
-    path('stories/create/success/', views.StoryCreateSuccessTemplateView.as_view(), name='story-create-success'),
+    path('stories/',
+         views.StoryListView.as_view(),
+         name='story-list'),
+    path('stories/<int:pk>/',
+         views.StoryDetailView.as_view(),
+         name='story-detail'),
+    path('stories/create/',
+         views.StoryCreateView.as_view(),
+         name='story-create'),
+    path('stories/create/success/',
+         views.StoryCreateSuccessTemplateView.as_view(),
+         name='story-create-success'),
     # Participant
-    path('participant/', views.ParticipantCreateView.as_view(), name='participant-create'),
-    path('participant/success/', views.ParticipantCreateSuccessTemplateView.as_view(), name='participant-create-success'),
+    path('participant/',
+         views.ParticipantCreateView.as_view(),
+         name='participant-create'),
+    path('participant/success/',
+         views.ParticipantCreateSuccessTemplateView.as_view(),
+         name='participant-create-success'),
 ]
