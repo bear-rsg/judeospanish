144545d7be600ceaea363301673e97096e1d92bc
Michael Allaway <m.j.allaway@bham.ac.uk>
2021-06-29 10:09:35 +0100

    participant dev

--- django/researchdata/urls.py
+++ django/researchdata/urls.py
@@ -16,10 +16,10 @@ urlpatterns = [
          views.StoryCreateSuccessTemplateView.as_view(),
          name='story-create-success'),
     # Participant
-    path('participant/',
+    path('getinvolved/',
          views.ParticipantCreateView.as_view(),
          name='participant-create'),
-    path('participant/success/',
+    path('getinvolved/success/',
          views.ParticipantCreateSuccessTemplateView.as_view(),
          name='participant-create-success'),
 ]
